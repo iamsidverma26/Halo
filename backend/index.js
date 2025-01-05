aeaf0ebc0cc97027feb4d53a7dcffcc2d3ff9be9
iamsidverma26 <20BCS9848@cuchd.in>
2025-01-05 18:29:27 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption ={
-    origin:'http://localhost:5173',
+    origin:'http://localhost:5174',
     credentials:true
 };
 app.use(cors(corsOption));
